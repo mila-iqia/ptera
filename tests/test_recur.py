5bc260dee0647d66cf1b45e101ba9e1ceece6712
Olivier Breuleux <breuleux@gmail.com>
2020-02-28 16:59:19 -0500

    Test Recurrence representation

--- tests/test_recur.py
+++ tests/test_recur.py
@@ -20,3 +20,13 @@ def test_recurrence():
         rq[0]
     assert rq[1] == 3
     assert rq[2] == 4
+
+
+def test_recurrence_repr():
+    rq = Recurrence(2)
+    assert str(rq) == "Recurrence(2, {})"
+    rq[0] = 1
+    rq[1] = 2
+    rq[2] = 3
+    rq[3] = 4
+    assert str(rq) == "Recurrence(2, {2: 3, 3: 4})"
