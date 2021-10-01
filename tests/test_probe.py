9710933e19745365bc3695fb6274ab43d7349395
Olivier Breuleux <breuleux@gmail.com>
2021-10-01 01:20:47 -0400

    Update probe tests

--- tests/test_probe.py
+++ tests/test_probe.py
@@ -1,39 +1,11 @@
 import sys
-from operator import itemgetter
 
 import pytest
 import rx
 
-from ptera import op
 from ptera.probe import Probe, probing
 
 from .milk import cheese as ch, gouda
-
-
-class Accumulator:
-    def __init__(self, fn=None):
-        self.fn = fn
-        self.results = []
-
-    def clear(self):
-        self.results.clear()
-
-    def check(self, expected):
-        if isinstance(expected, set):
-            assert set(self.results) == expected
-        else:
-            assert self.results == list(expected)
-
-    def on_next(self, data):
-        if self.fn:
-            data = self.fn(data)
-        self.results.append(data)
-
-    def on_error(self, err):
-        raise err
-
-    def on_completed(self):
-        pass
 
 
 def f(x):
@@ -71,146 +43,94 @@ def nitrogen(n):
         yield j
 
 
-def test_probe_raw():
-    results = Accumulator(lambda data: data["a"].value)
-    probe = Probe("f > a", raw=True)
-
-    probe.subscribe(results)
-
-    loopy()
-    results.check(x * x for x in range(100))
-
-    results.clear()
-    probe.__exit__()
-    loopy()
-    results.check([])
-
-
 def test_probe():
-    results = Accumulator(itemgetter("a"))
     probe = Probe("f > a")
-
-    probe.subscribe(results)
-
+    results = probe["a"].accum()
     loopy()
-    results.check(x * x for x in range(100))
+    assert results == [x * x for x in range(100)]
+
+    probe.__exit__()
 
     results.clear()
-    probe.__exit__()
     loopy()
-    results.check([])
+    assert results == []
+
+
+def test_probe_raw():
+    with probing("f > a", raw=True) as probe:
+        results = probe["a"].map(lambda x: x.value).accum()
+        loopy()
+        assert results == [x * x for x in range(100)]
+
+    results.clear()
+    loopy()
+    assert results == []
 
 
 def test_probe_method():
-    results = Accumulator(itemgetter("wow"))
-    probe = Probe("Elephant.sing > wow")
+    with probing("Elephant.sing > wow") as probe:
+        results = probe["wow"].accum()
+        loopy()
 
-    probe.subscribe(results)
-
-    loopy()
-    results.check(x + 10 for x in range(100))
-
-    results.clear()
-    probe.__exit__()
-    loopy()
-    results.check([])
+    assert results == [x + 10 for x in range(100)]
 
 
 def test_probe_tag():
-    results = Accumulator(itemgetter("x"))
-    probe = Probe("f > $x:@flag")
+    with probing("f > $x:@flag") as probe:
+        results = probe["x"].accum()
+        loopy()
 
-    probe.subscribe(results)
-
-    loopy()
-    results.check(x * x for x in range(100))
-
-    results.clear()
-    probe.__exit__()
-    loopy()
-    results.check([])
+    assert results == [x * x for x in range(100)]
 
 
 def test_pipe():
-    results = Accumulator()
-    probe = Probe("f > a")
+    with probing("f > a") as probe:
+        neg = probe.kmap(lambda a: -a)
+        results = neg.accum()
+        loopy()
 
-    neg = probe.pipe(op.map(lambda data: -data["a"]))
-
-    neg.subscribe(results)
-
-    loopy()
-    results.check(-x * x for x in range(100))
-
-    probe.__exit__()
+    assert results == [-x * x for x in range(100)]
 
 
 def test_merge():
-    results = Accumulator()
-    probe = Probe("f > a")
+    with probing("f > a") as probe:
+        arr = rx.of(-1, -2, -3)
+        merged = probe["a"] | arr
+        results = merged.accum()
 
-    arr = rx.of(-1, -2, -3)
-    vals = probe.pipe(op.map(itemgetter("a")))
-    merged = arr.pipe(op.merge(vals))
+        loopy()
 
-    merged.subscribe(results)
+    assert set(results) == {x * x for x in range(100)} | {-1, -2, -3}
+
+
+def test_two_probes():
+    probe1 = Probe("f > a")
+    probe2 = Probe("g > a")
+
+    results1 = probe1["a"].accum()
+    results2 = probe2["a"].accum()
 
     loopy()
-    results.check({x * x for x in range(100)} | {-1, -2, -3})
+    assert results1 == [x * x for x in range(100)]
+    assert results2 == [-x for x in range(100)]
 
-    probe.__exit__()
-
-
-# def test_two_probes():
-#     results1 = Accumulator(itemgetter("a"))
-#     results2 = Accumulator(itemgetter("a"))
-
-#     probe1 = Probe("f > a")
-#     probe2 = Probe("g > a")
-
-#     probe1.subscribe(results1)
-#     probe2.subscribe(results2)
-
-#     probe2.deactivate()
-#     loopy()
-#     results1.check([x * x for x in range(100)])
-#     results2.check([])
-
-#     probe2.activate()
-#     loopy()
-#     results1.check([x * x for x in range(100)] * 2)
-#     results2.check([-x for x in range(100)])
-
-#     probe1.deactivate()
-#     loopy()
-#     results1.check([x * x for x in range(100)] * 2)
-#     results2.check([-x for x in range(100)] * 2)
+    probe1.__exit__()
+    probe2.__exit__()
 
 
-# def test_probe_same_var_twice():
-#     results1 = Accumulator(itemgetter("a"))
-#     results2 = Accumulator(itemgetter("a"))
+def test_probe_same_var_twice():
+    probe1 = Probe("f > a")
+    results1 = probe1["a"].accum()
 
-#     probe1 = Probe("f > a")
-#     probe2 = Probe("f > a")  # Different probe for same var
+    loopy()
+    assert results1 == [x * x for x in range(100)]
 
-#     probe1.subscribe(results1)
-#     probe2.subscribe(results2)
+    probe2 = Probe("f > a")  # Different probe for same var
+    results2 = probe2["a"].accum()
 
-#     probe2.deactivate()
-#     loopy()
-#     results1.check([x * x for x in range(100)])
-#     results2.check([])
-
-#     probe2.activate()
-#     loopy()
-#     results1.check([x * x for x in range(100)] * 2)
-#     results2.check([x * x for x in range(100)])
-
-#     probe1.deactivate()
-#     loopy()
-#     results1.check([x * x for x in range(100)] * 2)
-#     results2.check([x * x for x in range(100)] * 2)
+    loopy()
+    assert results1 == [x * x for x in range(100)] * 2
+    assert results2 == [x * x for x in range(100)]
 
 
 def test_bad_probe():
@@ -218,40 +138,20 @@ def test_bad_probe():
         Probe("unknown > a")
 
 
-# def test_local_probe():
-#     results = Accumulator()
-#     lp = LocalProbe("f > a").pipe(op.map(itemgetter("a")), op.max())
-#     lp.subscribe(results)
-#     with lp as probe:
-#         assert probe._local_probe is lp
-#         loopy()
-#     results.check([99 ** 2])
-
-#     results.clear()
-#     loopy()  # Should not accumulate
-
-#     with lp:
-#         # lp should be reusable
-#         loopy()
-#     results.check([99 ** 2])
-
-
 def test_probing():
-    results = Accumulator()
     with probing("f > a") as probe:
-        probe.pipe(op.map(itemgetter("a")), op.max()).subscribe(results)
+        results = probe["a"].max().accum()
         loopy()
-    results.check([99 ** 2])
+    assert results == [99 ** 2]
 
 
 def test_probing_generator():
-    results = Accumulator()
     with probing("nitrogen > j") as probe:
-        probe.pipe(op.map(itemgetter("j"))).subscribe(results)
+        results = probe["j"].accum()
         for x in nitrogen(10):
             if x > 10:
                 break
-    results.check([0, 1, 4, 9, 16])
+    assert results == [0, 1, 4, 9, 16]
 
 
 def test_probing_format(capsys):
@@ -275,12 +175,11 @@ def test_reactivate():
     sys.version_info < (3, 8), reason="requires python3.8 or higher"
 )
 def test_slash_probe():
-    results = Accumulator()
     with probing("/tests.milk/cheese > a") as probe:
-        probe.pipe(op.map(itemgetter("a"))).subscribe(results)
+        results = probe["a"].accum()
         assert ch(4) == 16 + 4
         assert ch(5) == 25 + 4
-    results.check([16, 25])
+    assert results == [16, 25]
 
 
 @pytest.mark.skipif(
@@ -290,23 +189,20 @@ def test_slash_probe_multi():
     # Probing a function rewrites it and gives it a new code object, so
     # we need to check if we can find multiple times reliably
 
-    results = Accumulator()
     with probing("/tests.milk/gouda > a") as probe:
-        probe.pipe(op.map(itemgetter("a"))).subscribe(results)
+        results = probe["a"].accum()
         assert gouda(4) == 64
-    results.check([8])
+    assert results == [8]
 
-    results = Accumulator()
     with probing("/tests.milk/gouda > b") as probe:
-        probe.pipe(op.map(itemgetter("b"))).subscribe(results)
+        results = probe["b"].accum()
         assert gouda(4) == 64
-    results.check([64])
+    assert results == [64]
 
-    results = Accumulator()
     with probing("/tests.milk/gouda() as ret") as probe:
-        probe.pipe(op.map(itemgetter("ret"))).subscribe(results)
+        results = probe["ret"].accum()
         assert gouda(4) == 64
-    results.check([64])
+    assert results == [64]
 
 
 def test_accumulate():
